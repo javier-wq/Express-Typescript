df1fc5c854c8b5a9b012c28b0dd406b2c4a60d6c
javier-wq <javiermonge247@gmail.com>
2023-11-01 11:09:14 -0600

    creating routes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import express from 'express'
+import express from 'express' //ESModules
+
+import diaryRouter from './routes/diaries'
+
 const app = express()
 app.use(express.json()) // middleware que transforma la req.body a un json.
 
@@ -9,6 +12,8 @@ app.get('/ping', (_req, res) => {
     console.log('Someone pinged here !! ' + new Date().toLocaleDateString())
     res.send('pong')
 })
+
+app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
